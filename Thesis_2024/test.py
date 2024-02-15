0906e7f823ca487a0274f0f36fd5898f148db46f
alelatt <alelatt2000@gmail.com>
2024-02-15 20:25:46 +0100

    Additional Tests

--- Thesis_2024/test.py
+++ Thesis_2024/test.py
@@ -1,1 +1,25 @@
-a = 2
+import rebound
+import time
+import matplotlib.pyplot as plt
+
+sim = rebound.Simulation()
+sim.start_server(port=1234)
+sim.units = ['mearth', 'day', 'AU']
+
+time.sleep(5)
+
+sim.add('Sun')
+sim.add('Earth')
+sim.add('Moon', primary = 'Earth')
+
+#sim.dt = 1
+sim.usleep = 100000
+#sim.integrator = 'whfast'
+sim.move_to_com()
+
+sim.status()
+time.sleep(5)
+sim.integrate(400)
+
+rebound.OrbitPlot(sim)
+plt.show()
