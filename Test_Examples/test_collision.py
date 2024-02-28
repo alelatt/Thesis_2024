43a66b5200b1c42c9cd2c6a59d524c574eb98e49
alelatt <alelatt2000@gmail.com>
2024-02-28 15:41:39 +0100

    Modified collision

--- Test_Examples/test_collision.py
+++ Test_Examples/test_collision.py
@@ -5,9 +5,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
+def Distance(sim, index1, index2):
+	pos1 = np.array([sim.particles[index1].x, sim.particles[index1].y, sim.particles[index1].z])
+	pos2 = np.array([sim.particles[index2].x, sim.particles[index2].y, sim.particles[index2].z])
+	return np.linalg.norm(pos2 - pos1)
+
 if __name__ == '__main__':
 	sa = rebound.Simulationarchive("./simarchive/archive12.bin")
-	print(sa[-1].t, Compute_Energy(sa[-1]))
 
 	rebound.OrbitPlot(sa[-1])
 	plt.show()
@@ -19,15 +23,18 @@ if __name__ == '__main__':
 	sim.ri_mercurius.L = "infinity"
 	sim.start_server(port=1234)
 
-	times = np.linspace(-523629, -523630, 10)
+	times = np.linspace(-523629, -523630, 1000)
+	dist = np.zeros(1000)
 
-	for time in times:
+	for i, time in enumerate(times):
 		sim.integrate(time, exact_finish_time = 0)
-		ob1 = rebound.OrbitPlot(sim)
-		plt.show()
-		plt.close('all')
+		dist[i] = Distance(sim, 1, 2)
 
 	ob1 = rebound.OrbitPlot(sim)
+
+	plt.figure("Distance")
+	plt.plot(dist)
+
 	plt.show()
 	plt.close('all')
 
